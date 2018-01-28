bd2774f4a27f2ab8e43aff01eee8bffc7a6c1cfe
Ivan Čurić <ivan.curic@broj42.com>
2018-01-28 19:30:46 +0100

    switched hover to click

--- src/js/images.js
+++ src/js/images.js
@@ -7,6 +7,8 @@ class ImageSet {
     this.imgItems = [...document.querySelectorAll('.img-item')];
     this.images = [...document.querySelectorAll('.img')];
 
+    this.state = 'collapsed';
+
     this.preload().then(_ => this.init());
   }
 
@@ -32,18 +34,20 @@ class ImageSet {
     this.imgSet.classList.remove('expanded'); // And return to normal
     this.imgSet.classList.add('loaded'); // fade the images in
 
-    this.imgSet.addEventListener('click', () => this.animate());
+    this.imgSet.addEventListener('click', () => this.animateIn());
+    // this.imgSet.addEventListener('mouseleave', () => this.animateOut());
   }
 
   /**
-   * A "FLIP" animation technique (https://aerotwist.com/blog/flip-your-animations/)
+   * FLIP (https://aerotwist.com/blog/flip-your-animations/)
    */
-  animate() {
+  animateIn() {
     this.imgSet.classList.add('animatable', 'flat');
 
-    // feeding an array of promises to resolve to accomodate for mixed or staggered transition durations
+    // an array of promises to accomodate for mixed or staggered transition durations
     const animationPromises = this.imgItems.map((img, i) => {
-      img.style.transform = `rotate(0deg) translateY(${this.rectInfo[i].y - 60}px)`; // magic number (padding + margin)
+      img.style.transform = `rotate(0deg) translateY(${this.rectInfo[i].y -
+        60}px)`; // magic number (padding + margin)
       return listenOnce(img, 'transitionend');
     });
     // when all elements are done animating

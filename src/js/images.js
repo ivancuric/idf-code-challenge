6695f59b1d30a7928dc888866542ea042ab67397
Ivan Čurić <ivan.curic@broj42.com>
2018-01-29 17:21:53 +0100

    wip refactor

--- src/js/images.js
+++ src/js/images.js
@@ -7,8 +7,6 @@ class ImageSet {
     this.imgItems = [...document.querySelectorAll('.img-item')];
     this.images = [...document.querySelectorAll('.img')];
 
-    this.state = 'collapsed';
-
     this.preload().then(_ => this.init());
   }
 
@@ -23,40 +21,65 @@ class ImageSet {
   // "Boots" up the image set
   async init() {
     window.scroll(0, 0);
+    this.imgSet.classList.add('is-loaded'); // fade the images in
+    this.imgSet.addEventListener('click', this.onClick);
+  }
+
+  /**
+   * Click handler
+   * Stage 1 arrow method to bind to `this`
+   * @param {Event} event
+   */
+  onClick = event => {
+    event.preventDefault();
+
+    // early exit
+    if (this.imgSet.classList.contains('is-animating')) {
+      return;
+    }
+
+    if (this.imgSet.classList.contains('is-expanded')) {
+      this.animateOut();
+    } else {
+      this.animateIn();
+    }
+  };
+
+  /**
+   * FLIP (https://aerotwist.com/blog/flip-your-animations/)
+   */
+  async animateIn() {
     await rafPromise(); // wait for a fresh frame to do the work
-    this.imgSet.classList.remove('animatable'); // in case of reinitialization
-    this.imgSet.classList.add('expanded'); // render the expanded state under a frame
+    this.imgSet.classList.add('is-expanded'); // render the is-expanded state under a frame
 
     // Get original non-rotated element dimensions
     // This is needed because not all images have the same aspect ratio
     this.rectInfo = this.imgItems.map(img => img.getBoundingClientRect());
 
-    this.imgSet.classList.remove('expanded'); // And return to normal
-    this.imgSet.classList.add('loaded'); // fade the images in
+    this.imgSet.classList.remove('is-expanded'); // And return to normal
 
-    this.imgSet.addEventListener('click', () => this.animateIn());
-    // this.imgSet.addEventListener('mouseleave', () => this.animateOut());
-  }
 
-  /**
-   * FLIP (https://aerotwist.com/blog/flip-your-animations/)
-   */
-  animateIn() {
-    this.imgSet.classList.add('animatable', 'flat');
+    // START THE ANIMATION
+    this.imgSet.classList.add('is-animating', 'is-flat');
 
     // an array of promises to accomodate for mixed or staggered transition durations
     const animationPromises = this.imgItems.map((img, i) => {
       img.style.transform = `rotate(0deg) translateY(${this.rectInfo[i].y -
-        60}px)`; // magic number (padding + margin)
+        60}px)`; // "magic" number (padding + margin)
       return listenOnce(img, 'transitionend');
     });
     // when all elements are done animating
     Promise.all(animationPromises).then(async () => {
       await rafPromise(); // wait for a fresh frame to do the work
-      this.imgSet.classList.remove('animatable');
+      this.imgSet.classList.remove('is-animating');
       this.imgItems.forEach(img => img.removeAttribute('style'));
-      this.imgSet.classList.add('expanded');
+      this.imgSet.classList.add('is-expanded');
     });
+  }
+
+  async animateOut() {
+    await rafPromise();
+    this.imgSet.classList.remove('is-expanded');
   }
 }
 
